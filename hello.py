b27f68afdeed3f783603a9fcde12a63b80ee1ec4
swath-I-1 <sweetychinni40@gmail.com>
2025-03-04 19:13:04 +0530

    palindrome code added to hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,17 @@
 print("HELLO WORLD")
 for i in range(1,10):
 	print(i)
+
+print("adding new things into these files")
+
+n=131
+temp=n;
+d=0;
+while(n>0):
+	k=n%10
+	d=d*10+k
+	n=n//10	
+if(d==temp):
+	print("palindrome")
+else:
+	print("not a palindrome")	
