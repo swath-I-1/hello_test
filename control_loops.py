4f5e95d3342f841c03bfa71ea0687e0193c89dfc
swath-I-1 <sweetychinni40@gmail.com>
2025-03-11 06:23:08 +0530

    control statements commited

---
+++ control_loops.py
@@ -0,0 +1,46 @@
+#PRINTING THE HELLO WORLD AS X times
+
+'''x=int(input("ENTER the  number"))
+while(x):
+	print("HELLO WORLD")
+	x=x-1 
+
+x=int(input("ENTER THE VALUE"))
+for i in range(x):
+	print("HELLO WORLD") '''
+
+
+#SUM OF ODD NUMBERS UPTO N
+
+'''n=int(input("ENTER THE NUMBER"))
+x=0
+for i in range(n+1):
+	if(i%2!=0):
+		x=x+i
+print("SUM OF ODD NUMBERS UPTO N IS",x)'''
+
+#NUMBER OF FACTORS FOR A NUMBER
+
+'''n=int(input("ENTER THE NUMBER"))
+c=0
+for i in range(1,n+1,1):
+	if(n%i==0):
+		c=c+1
+print("THE NUMBER OF FACTORS IS ",c)'''
+
+#NUMBER IS PRIME OR NOT
+
+'''n=int(input("ENTER THE NUMBER"))
+flag=0
+i=2	
+for i in range(2,i*i):
+	if(n%i==0):
+		flag=1
+		break
+if(flag==1):
+	print("NOT A PRIME NUMBER")
+else:
+	print("PRIME NUMBER")	'''
+	
+												
+	
